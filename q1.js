0adade46cf9746f9b2d56c3544faae5f5b32493e
corentin <corentin.martinot@gmail.com>
2020-02-04 12:32:44 +0100

    question 4

--- q1.js
+++ q1.js
@@ -1,6 +1,6 @@
 const event = {
-  startDate: '2019-04-13T09:00:00',
-  endDate: '2019-04-13T11:30:00',
+  startDate: '2019-04-11T09:00:00',
+  endDate: '2019-04-11T11:30:00',
   customer: '1234567890',
 };
 
@@ -19,6 +19,7 @@ const customer = {
       { startDate: '2019-04-15T00:00:00', unitTTCPrice: 22 },
     ],
   },
+  funding: { percentage: 60, thirdPartyPayer: 'Hauts de Seine' },
 };
 
 function diff_minutes(dt2, dt1)
@@ -46,13 +47,17 @@ let event_price = function(event){
   const cus = customer; //dans un cas classique on interrogerait une BD pour avoir les infos du bon beneficiaire 'event.customer', ici on va simplement chercher la variable globale 'customer'
   let startDateObject = new Date(event.startDate);
   let endDateObject = new Date(event.endDate);
-  let total = diff_minutes(startDateObject,endDateObject) / 60 * subscriptionCurentVersion(cus.subscription,event.startDate).unitTTCPrice;
-  if(startDateObject.getDay()==0){
-    total*=(100+cus.subscription.service.sundaySurcharge)/100;
-  } else if (startDateObject.getDay()==6) {
-    total*=(100+cus.subscription.service.saturdaySurcharge)/100;
+  let chargeCustomer = diff_minutes(startDateObject,endDateObject) / 60 * subscriptionCurentVersion(cus.subscription,event.startDate).unitTTCPrice;
+  if(startDateObject.getDay()==0){  // le dimanche
+    chargeCustomer*=(100+cus.subscription.service.sundaySurcharge)/100;
+  } else if (startDateObject.getDay()==6) { // le samedi
+    chargeCustomer*=(100+cus.subscription.service.saturdaySurcharge)/100;
+  } else { // en semaine
+    if (cus.funding!= undefined){
+      return [chargeCustomer*(100-cus.funding.percentage)/100, chargeCustomer*cus.funding.percentage/100]
+    }
   }
-  return total;
+  return [chargeCustomer,0]; //[chargeCustomer, chargeFunder]
 }
 
 console.log(subscriptionCurentVersion(customer.subscription,'2019-03-15T00:00:00'));

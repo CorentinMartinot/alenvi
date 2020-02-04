33c98be906f3e7b20eb8c476aebbe00a861fae92
corentin <corentin.martinot@gmail.com>
2020-02-04 12:56:29 +0100

    question 5

--- q1.js
+++ q1.js
@@ -2,12 +2,13 @@ const event = {
   startDate: '2019-04-11T09:00:00',
   endDate: '2019-04-11T11:30:00',
   customer: '1234567890',
+  subscription: 'asdfghjkl',
 };
 
 const customer = {
   _id: '1234567890',
   identity : { lastname : 'X' },
-  subscription: {
+  subscriptions: [{
     _id: 'qwertyuio',
     service: {
       name: 'Aide a l\'autonomie',
@@ -18,8 +19,19 @@ const customer = {
       { startDate: '2019-02-01T00:00:00', unitTTCPrice: 24 },
       { startDate: '2019-04-15T00:00:00', unitTTCPrice: 22 },
     ],
-  },
-  funding: { percentage: 60, thirdPartyPayer: 'Hauts de Seine' },
+  }, {
+    _id: 'asdfghjkl',
+    service: { name: 'Ménage' },
+    versions: [
+      { startDate: '2019-02-01T00:00:00', unitTTCPrice: 22.3 },
+      { startDate: '2019-04-15T00:00:00', unitTTCPrice: 21.7 },
+    ],
+  }],
+  fundings: [{
+    subscription: 'qwertyui',
+    percentage: 60,
+    thirdPartyPayer: 'Hauts de Seine',
+  }],
 };
 
 function diff_minutes(dt2, dt1)
@@ -43,23 +55,34 @@ let subscriptionCurentVersion = function(subscription, date){
   }
 };
 
+let findSubscription = function (customer, subscriptionId){
+  return customer.subscriptions.find(x => x._id === subscriptionId);
+}
+
+let findFunding = function (customer, subscriptionId){
+  return customer.fundings.find(x => x.subscription === subscriptionId);
+}
+
 let event_price = function(event){
   const cus = customer; //dans un cas classique on interrogerait une BD pour avoir les infos du bon beneficiaire 'event.customer', ici on va simplement chercher la variable globale 'customer'
   let startDateObject = new Date(event.startDate);
   let endDateObject = new Date(event.endDate);
-  let chargeCustomer = diff_minutes(startDateObject,endDateObject) / 60 * subscriptionCurentVersion(cus.subscription,event.startDate).unitTTCPrice;
+  let chargeCustomer = diff_minutes(startDateObject,endDateObject) / 60 * subscriptionCurentVersion(findSubscription(cus,event.subscription),event.startDate).unitTTCPrice;
+
   if(startDateObject.getDay()==0){  // le dimanche
     chargeCustomer*=(100+cus.subscription.service.sundaySurcharge)/100;
   } else if (startDateObject.getDay()==6) { // le samedi
     chargeCustomer*=(100+cus.subscription.service.saturdaySurcharge)/100;
   } else { // en semaine   -> ca n'apparait pas dans le JSON mais dans l'énoncé on nous dit que les aides ne sont donné qu'en semaine...
-    if (cus.funding!= undefined){
-      return [chargeCustomer*(100-cus.funding.percentage)/100, chargeCustomer*cus.funding.percentage/100]
+    let fundingPercentage = findFunding(cus, event.subscription);
+    if (fundingPercentage != undefined){
+      return [chargeCustomer*(100-fundingPercentage.percentage)/100, chargeCustomer*fundingPercentage.percentage/100]
     }
   }
   return [chargeCustomer,0]; //[chargeCustomer, chargeFunder]
 }
 
-console.log(subscriptionCurentVersion(customer.subscription,'2019-03-15T00:00:00'));
-console.log(subscriptionCurentVersion(customer.subscription,'2019-05-15T00:00:00'));
+
+console.log(findSubscription(customer,event.subscription));
+console.log(findFunding(customer, event.subscription));
 console.log(event_price(event));
